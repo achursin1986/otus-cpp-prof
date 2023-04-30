77c46dfe3436f68591b2447ac91bda0854ebffb1
achursin1986 <achursin1986@gmail.com>
2023-04-30 02:01:28 +0200

    HW9

---
+++ async.cpp
@@ -0,0 +1,43 @@
+#include "async.h"
+#include <mutex>
+#include <unordered_map>
+#include "processor.hpp"
+#include "threadpool.hpp"
+#include <cstring>
+
+
+
+
+namespace async {
+
+handle_t connect(std::size_t bulk) {
+     
+    std::lock_guard<std::mutex> lock(my::mtx);
+    //std::unique_ptr<my::Combi> c = std::make_unique<my::Combi>(bulk);
+    std::shared_ptr<my::Combi> c( new my::Combi(bulk));  
+    void * p = c.get();  
+    my::contexts[p] = c;
+
+
+    return my::contexts[p].get();    
+        // return nullptr;
+}
+
+void receive(handle_t handle, const char *data, std::size_t size) { 
+    //std::unique_ptr<char[]> s (new char[size] );
+    std::shared_ptr<char[]> s (new char[size] );
+    std::memcpy(s.get(), data ,size);  
+    Scheduler.schedule(my::Receive(handle,s,size));
+}
+
+
+
+void disconnect(handle_t handle) {
+     // find context, make save  before destruction
+     std::lock_guard<std::mutex> lock(my::mtx);
+     my::contexts.erase(handle);
+
+}
+
+}
+
